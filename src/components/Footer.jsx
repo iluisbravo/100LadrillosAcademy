69bcdbd513b566fbf84c5af7e6583b6fcde9d7b9
César Romero <cesar.romero@100ladrillos.com>
2024-03-15 01:10:30 -0600

    Modify footer to add social media and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,68 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <footer className="p-5">
       <div className="row">
         <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
+          <h5>Academy</h5>
           <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                <i className="bi bi-mortarboard-fill"/> Cursos
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                <i className="bi bi-people-fill"/> Comunidades
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                <i className="bi bi-webcam-fill"/> Webinars
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+              <i className="bi bi-box2-heart-fill"/> Recursos
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                <i className="bi bi-patch-question-fill"/> FAQs
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                <i className="bi bi-info-circle-fill"/> Acerca De
+              </a>
+            </li>
           </ul>
         </div>
 
         <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
+          <h5>Comunidad</h5>
           <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Principales Foros de Ayuda</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Foros Populares</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Recien Iniciados</a></li>
           </ul>
         </div>
 
         <div className="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
+          <h5>Otros</h5>
           <ul className="nav flex-column">
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Términos y Condiciones</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Aviso de privacidad</a></li>
+            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Contacto</a></li>
           </ul>
         </div>
 
         <div className="col-md-5 offset-md-1 mb-3">
           <form>
-            <h5>Subscribe to our newsletter</h5>
-            <p>Monthly digest of what's new and exciting from us.</p>
+            <h5>Subscribete a nuestro newsletter</h5>
             <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-              <label className="visually-hidden">Email address</label>
-              <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-              <button className="btn btn-primary" type="button">Subscribe</button>
+              <label className="visually-hidden">Dirección de Correo</label>
+              <input id="newsletter1" type="text" className="form-control" placeholder="Dirección de Correo" />
+              <button className="btn btn-primary" type="button">Subscribirse</button>
             </div>
           </form>
         </div>
--- src/components/SubFooter.jsx
+++ src/components/SubFooter.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-
 const SubFooter = () => {
   return (
-    <div className="d-flex flex-column flex-sm-row justify-content-between p-5">
-    <p>Code with <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" className="bi bi-heart-fill" viewBox="0 0 16 16">
-      <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
-    </svg> in 100 Ladrillos Sonata, Guadalajara.</p>
-    <ul className="list-unstyled d-flex">
-      <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlink: href="#twitter"></use></svg></a></li>
-      <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlink: href="#instagram"></use></svg></a></li>
-      <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlink: href="#facebook"></use></svg></a></li>
-    </ul>
-  </div>   
+    <div className="text-center p-5">
+      <p>Code <i className="bi bi-code-slash"/> with &nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" className="bi bi-heart-fill" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+        </svg>
+        &nbsp; in 100 Ladrillos Sonata, Guadalajara, MX.
+      </p>
+
+      <div className="social-icons">
+        <ul className="list-unstyled d-flex justify-content-end">
+          <li className="ms-3">
+            <a className="link-body-emphasis" href="https://www.facebook.com/100Ladrillos">
+              <i className="bi bi-facebook fs-3"/>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="link-body-emphasis" href="https://www.instagram.com/100ladrillos">
+              <i className="bi bi-instagram fs-3"/>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="link-body-emphasis" href="https://twitter.com/100ladrillos">
+              <i className="bi bi-twitter-x fs-3"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+  </div>
   )
 }
 
